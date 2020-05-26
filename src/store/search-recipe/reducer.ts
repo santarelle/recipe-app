351e9b4d0b6b97e46b1ae99798bf81e6702f4807
Marco Santarelle da Silva Jardim <santarelle@gmail.com>
2020-05-26 16:07:34 -0400

    feat(redux): add search-recipe redux

---
+++ src/store/search-recipe/reducer.ts
@@ -0,0 +1,32 @@
+import {
+  SearchRecipeState,
+  SearchRecipeActionTypes,
+  SEARCH_REQUEST,
+  SEARCH_SUCCESS,
+} from './types';
+
+const INITIAL_STATE: SearchRecipeState = {
+  term: '',
+  recipes: [],
+  loading: false,
+};
+
+export const searchRecipe = (
+  state = INITIAL_STATE,
+  action: SearchRecipeActionTypes,
+): SearchRecipeState => {
+  switch (action.type) {
+    case SEARCH_REQUEST:
+      return {
+        ...state,
+        term: action.payload.term,
+        loading: true,
+      };
+
+    case SEARCH_SUCCESS:
+      return { ...state, recipes: action.payload.recipes, loading: false };
+
+    default:
+      return { ...state };
+  }
+};
