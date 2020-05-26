e122264c2085c55b7366a8eb9792e41d611e4873
Marco Santarelle da Silva Jardim <santarelle@gmail.com>
2020-05-26 15:59:25 -0400

    feat: add loading component

---
+++ src/components/loading/Loading.tsx
@@ -0,0 +1,11 @@
+import './Loading.scss';
+
+import React from 'react';
+
+import CircularProgress from '@material-ui/core/CircularProgress';
+
+export const Loading = () => (
+  <div className="loading-container">
+    <CircularProgress size={100} />
+  </div>
+);
