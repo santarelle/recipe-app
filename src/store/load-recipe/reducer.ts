4a90e71eefada4c47752f3ac379ff5872d8ab336
Marco Santarelle da Silva Jardim <santarelle@gmail.com>
2020-05-26 16:07:08 -0400

    feat(redux): add load-recipe redux

---
+++ src/store/load-recipe/reducer.ts
@@ -0,0 +1,27 @@
+import {
+  LoadRecipeState,
+  LoadRecipeActionTypes,
+  LOAD_REQUEST,
+  LOAD_SUCCESS,
+} from 'src/store/load-recipe/types';
+
+const INITIAL_STATE: LoadRecipeState = {
+  recipe: undefined,
+  loading: false,
+};
+
+export const loadRecipe = (
+  state = INITIAL_STATE,
+  action: LoadRecipeActionTypes,
+): LoadRecipeState => {
+  switch (action.type) {
+    case LOAD_REQUEST:
+      return { ...state, loading: true };
+
+    case LOAD_SUCCESS:
+      return { ...state, loading: false, recipe: action.payload.recipe };
+
+    default:
+      return { ...state };
+  }
+};
