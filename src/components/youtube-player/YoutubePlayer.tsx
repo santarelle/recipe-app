91ed987910c0cdf29d6a5126d2391bf6a29ae8a5
Marco Santarelle da Silva Jardim <santarelle@gmail.com>
2020-05-26 15:57:50 -0400

    feat: add a component to show youtube embed video

---
+++ src/components/youtube-player/YoutubePlayer.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+
+type YoutubePlayerProps = {
+  title: string;
+  embedUrl: string;
+  width?: string;
+  height?: string;
+};
+
+export const YoutubePlayer: React.FC<YoutubePlayerProps> = (
+  props: YoutubePlayerProps,
+) => {
+  const { title, embedUrl, width, height } = props;
+
+  return (
+    <iframe
+      title={title}
+      width={width ?? '100%'}
+      height={height ?? '480'}
+      src={embedUrl}
+      frameBorder="0"
+      allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+      allowFullScreen
+    />
+  );
+};
