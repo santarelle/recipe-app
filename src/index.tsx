801331034ab6de9aaa5cf662cade723dfb8ca4a3
Marco Santarelle da Silva Jardim <santarelle@gmail.com>
2020-05-26 16:08:17 -0400

    refactor: change index.scss to global.scss

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,9 @@
-import './index.scss';
-
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import App from './App';
+import { App } from 'src/App';
+
+import './global.scss';
 
 ReactDOM.render(
   <React.StrictMode>
