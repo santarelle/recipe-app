74c5bee1976ce22bebd965410195c870922ca979
Marco Santarelle da Silva Jardim <santarelle@gmail.com>
2020-05-30 14:04:53 -0400

    style: improve UI for recipe preparations

--- src/components/recipe-detail/recipe-preparation/RecipePreparation.tsx
+++ src/components/recipe-detail/recipe-preparation/RecipePreparation.tsx
@@ -1,5 +1,9 @@
 import React from 'react';
 
+import Divider from '@material-ui/core/Divider';
+import Grid from '@material-ui/core/Grid';
+import Typography from '@material-ui/core/Typography';
+
 type RecipePreparationProps = {
   preparations: string[];
 };
@@ -9,6 +13,35 @@ export const RecipePreparation: React.FC<RecipePreparationProps> = (
 ) => {
   const { preparations } = props;
   return (
-    <ul>{preparations && preparations.map((i) => <li key={i}>{i}</li>)}</ul>
+    <div className="recipe-preparation-container">
+      <Typography variant="h4" component="h3">
+        Preparation
+      </Typography>
+
+      <Divider className="divider" />
+
+      <Grid container>
+        {preparations &&
+          preparations.map((step, idx) => (
+            <>
+              <Grid
+                item
+                md={2}
+                xs={12}
+                key={step}
+                style={{ paddingBottom: '20px' }}
+              >
+                <Typography variant="h4" component="h3">
+                  Step&nbsp;
+                  {idx + 1}
+                </Typography>
+              </Grid>
+              <Grid item md={10} style={{ paddingBottom: '20px' }}>
+                <Typography variant="body2">{step}</Typography>
+              </Grid>
+            </>
+          ))}
+      </Grid>
+    </div>
   );
 };
