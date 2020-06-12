b114347fcebe0e2d131cd6895ff69e40c7373bd6
Marco Santarelle da Silva Jardim <santarelle@gmail.com>
2020-06-12 15:19:06 -0400

    feat: add favorite button on recipe detail page

--- src/pages/recipe-detail/RecipeDetailPage.tsx
+++ src/pages/recipe-detail/RecipeDetailPage.tsx
@@ -1,28 +1,41 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { useLocation } from 'react-router-dom';
 
+import Button from '@material-ui/core/Button';
 import Divider from '@material-ui/core/Divider';
 import Grid from '@material-ui/core/Grid';
 import Paper from '@material-ui/core/Paper';
 import Typography from '@material-ui/core/Typography';
+import Favorite from '@material-ui/icons/Favorite';
+import FavoriteBorder from '@material-ui/icons/FavoriteBorder';
 
 import { Loading } from 'src/components/loading/Loading';
 import { RecipeIngredient } from 'src/components/recipe-detail/recipe-ingredient/RecipeIngredient';
 import { RecipePreparation } from 'src/components/recipe-detail/recipe-preparation/RecipePreparation';
 import { YoutubePlayer } from 'src/components/youtube-player/YoutubePlayer';
+import FavoriteRecipeService from 'src/services/favorite-recipe.service';
 import { AppState } from 'src/store';
+import * as FavoriteRecipeActions from 'src/store/favorite-recipe/actions';
 import { loadRecipe } from 'src/store/load-recipe/thunks';
 
 import './RecipeDetailPage.scss';
 
 export const RecipeDetailPage: React.FC = () => {
   const { pathname } = useLocation();
+  const [isFavorite, setFavorite] = useState<boolean>(false);
 
   const dispatch = useDispatch();
   const { loading, recipe } = useSelector(
     (state: AppState) => state.loadRecipe,
   );
+  const favoriteRecipes = useSelector(
+    (state: AppState) => state.favoriteRecipes.recipes,
+  );
+
+  useEffect(() => {
+    setFavorite(favoriteRecipes.some((fr) => fr.title === recipe?.title));
+  }, [recipe, favoriteRecipes]);
 
   useEffect(() => {
     const titleUnique = pathname.split('/recipe/')[1];
@@ -30,15 +43,44 @@ export const RecipeDetailPage: React.FC = () => {
     dispatch(loadRecipe(title));
   }, [pathname, dispatch]);
 
+  const handleFavorite = () => {
+    if (recipe) {
+      if (isFavorite) {
+        FavoriteRecipeService.remove(recipe.id);
+        dispatch(FavoriteRecipeActions.removeFavoriteRecipe(recipe));
+      } else {
+        FavoriteRecipeService.save(recipe);
+        dispatch(FavoriteRecipeActions.saveFavoriteRecipe(recipe));
+      }
+    }
+  };
+
   return (
     <div className="recipe-detail-container">
       {loading ? (
         <Loading />
       ) : (
         <>
-          <Typography variant="h6" component="h2" color="primary">
-            {recipe?.title}
-          </Typography>
+          <Grid container justify="space-between">
+            <Typography variant="h6" component="h2" color="primary">
+              {recipe?.title}
+            </Typography>
+
+            <Button
+              variant={isFavorite ? 'contained' : 'outlined'}
+              color="primary"
+              onClick={handleFavorite}
+              startIcon={
+                isFavorite ? (
+                  <Favorite fontSize="large" />
+                ) : (
+                  <FavoriteBorder fontSize="large" />
+                )
+              }
+            >
+              {isFavorite ? 'Favorited' : 'Favorite'}
+            </Button>
+          </Grid>
 
           <Divider className="divider" />
 
